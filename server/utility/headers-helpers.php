9ff1ded0c70290cf1d81c536317f1bb915007d51
Parth <partharora99160808@gmail.com>
2020-09-12 12:58:31 +0530

    added cors headers

--- server/utility/headers-helpers.php
+++ server/utility/headers-helpers.php
@@ -3,4 +3,8 @@
 function addCommonHeaders()
 {
     header('Content-Type:Application/json');
+    header("Access-Control-Allow-Origin: *");
+    header('Access-Control-Allow-Credentials: true');
+    header("Access-Control-Allow-Methods: *");
+    header('Access-Control-Max-Age: 86400');
 }
