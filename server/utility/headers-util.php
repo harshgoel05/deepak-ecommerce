d1ec43d9ed270e49cf3127dcd7f591504e963919
Parth <partharora99160808@gmail.com>
2020-09-22 12:40:10 +0530

    added common origin

--- server/utility/headers-util.php
+++ server/utility/headers-util.php
@@ -3,8 +3,17 @@ namespace Utility\HeadersUtil;
 
 function addCommonHeaders()
 {
+    if (array_key_exists('HTTP_ORIGIN', $_SERVER)) {
+        $origin = $_SERVER['HTTP_ORIGIN'];
+    }
+    else if (array_key_exists('HTTP_REFERER', $_SERVER)) {
+        $origin = $_SERVER['HTTP_REFERER'];
+    } else {
+        $origin = $_SERVER['REMOTE_ADDR'];
+    }
+
     header('Content-Type:Application/json');
-    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Origin: $origin");
     header('Access-Control-Allow-Credentials: true');
     header("Access-Control-Allow-Methods: *");
     header('Access-Control-Max-Age: 86400');
