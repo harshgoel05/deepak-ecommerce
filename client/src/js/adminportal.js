238b650647fb0eea328ca3f8bb6bb866bc3bdfef
Aman Kumar <ak8804347052@gmail.com>
2020-09-27 10:35:09 +0530

    feat:ui_updates

--- client/src/js/adminportal.js
+++ client/src/js/adminportal.js
@@ -1,16 +1,16 @@
-$("#menu-toggle").click(function (e) {
+/*$("#menu-toggle").click(function(e) {
   e.preventDefault();
   $("#wrapper").toggleClass("toggled");
 });
 
 $(document).ready(function () {
-  $(".sec").hide();
-  $("#targetdiv1").show();
-  $("#selectMe").change(function () {
-    $(".sec").hide();
-    $("#" + $(this).val()).show();
-  });
-});
+  $('.sec').hide();
+  $('#targetdiv1').show();
+  $('#selectMe').change(function () {
+    $('.sec').hide();
+    $('#'+$(this).val()).show();
+  })
+});*/
 function initImageUpload(box) {
   let uploadField = box.querySelector(".image-upload");
 
