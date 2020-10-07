370be61c9378d33c5af81209f131672cb6d912f5
harshgoel05 <harshgoel05@gmail.com>
2020-10-07 10:32:26 +0530

    coupons

--- client/src/js/admin-add-coupons.js
+++ client/src/js/admin-add-coupons.js
@@ -1,26 +1,28 @@
 $("#admin-add-coupon-btn").on("click", function (event) {
   event.preventDefault();
-  var username = $("#username").val();
-  var pass = $("#pass").val();
-  console.log(username, pass);
-  if (username && pass) {
-    let body = {
-      username: username,
-      password: pass,
-    };
+  var form_data = $("#add_coupon_form").serializeArray();
+  var body_data = {};
+  let flag = 0;
+  for (let i = 0; i < form_data.length; i++) {
+    if (!form_data[i].value) {
+      flag = 1;
+      break;
+    } else {
+      body_data[form_data[i].name] = form_data[i].value;
+    }
+  }
+  if (flag != 0) {
     $.ajax({
       url:
-        "https://testing1.thestrategybook.com/deepak-ecommerce/server/api/admin/login.php",
+        "https://testing1.thestrategybook.com/deepak-ecommerce/server/api/user/add.php",
       type: "POST",
       data: JSON.stringify(body),
-      success: function (response, status, xhr) {
-        console.log(response);
-        window.location.href =
-          "https://testing1.thestrategybook.com/deepak-ecommerce/client/adminportal/index.html";
+      success: function (response) {
+        alert("User registerations successfull!");
       },
       error: function (xhr, status) {
         console.log("error", xhr, status);
-        alert("The username or password doesn't match");
+        alert("Some unknown error occured. Please try logging in.");
       },
     });
   } else {
