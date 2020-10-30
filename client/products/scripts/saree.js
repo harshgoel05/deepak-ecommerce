3e52784d352905861641570cb376f6c7f8258f39
Arjun <cosmicpepe2001@gmail.com>
2020-10-30 20:06:09 +0530

    cart script

--- client/products/scripts/saree.js
+++ client/products/scripts/saree.js
@@ -1,45 +1,103 @@
 function appendData(productElement) {
-
-    const card = document.createElement('div');
-    const body = document.createElement('div');
-    
-    const heading = document.createElement('h1');
-    const subtitle = document.createElement('h2');
-    const img = document.createElement('img');
-    const price = document.createElement('h3');
-    const qty = document.createElement('h3');
-
     const productBody = document.querySelector('.product_body');
-    heading.innerHTML = productElement.title;
+
+    const column = document.createElement('div');
+    column.classList.add('col-lg-4');
+    column.classList.add('col-md-6');
+
+    const listItem = document.createElement('div');
+    listItem.classList.add('single-product');
+    listItem.classList.add('list-item');
+
+    const img = document.createElement('img');
+    img.classList.add('img-fluid');
+    img.src = productElement.img1;
+
+    const productDetails = document.createElement('div');
+    productDetails.classList.add('product-details');
+    const title = document.createElement('h6');
+    const subtitle = document.createElement('h6');
     subtitle.innerHTML = productElement.subtitle;
-    img.innerHTML = productElement.image1;
-    price.innerHTML = productElement.price;
-    qty.innerHTML = productElement.quantity;
+    title.innerHTML = productElement.title;
+    productDetails.appendChild(title);
+    productDetails.appendChild(subtitle);
+    const priceBox = document.createElement('div');
+    priceBox.classList.add('price');
+    const priceVal = document.createElement('h6');
+    priceVal.innerHTML = 'RS ' + productElement.price;
+    priceBox.appendChild(priceVal);
+    productDetails.appendChild(priceBox);
 
-    card.appendChild(img);
-    body.appendChild(heading);
-    body.appendChild(subtitle);
-    body.appendChild(price);
-    body.appendChild(qty);
-    
-    card.appendChild(body);
 
-    card.classList.add("card");
-    img.classList.add("card-img-top");
-    body.classList.add("card-body");
-    heading.classList.add("card-title");
-    subtitle.classList.add("card-text");
+    const bottom = document.createElement('div');
+    bottom.classList.add('prd-bottom');
+    const cartButton = document.createElement('a');
+    cartButton.href = '#';
+    cartButton.classList.add('social-info');
+    cartButton.classList.add('cart-button');
+    const cartButtonIcon = document.createElement('i');
+    cartButton.id = productElement.productid;
+    cartButtonIcon.classList.add('fa');
+    cartButtonIcon.classList.add('fa-shopping-bag');
+    cartButtonIcon.style.color = "orange";
 
-    productBody.appendChild(card);
+    cartButton.appendChild(cartButtonIcon);
+    bottom.appendChild(cartButton);
+
+    listItem.appendChild(img);
+    listItem.appendChild(productDetails);
+    listItem.appendChild(bottom);
+
+    column.appendChild(listItem);
+    productBody.appendChild(column);
+
 }
 
 $(document).ready(async function () {
     const response = await fetch('https://testing1.thestrategybook.com/deepak-ecommerce/server/api/products/sarees/retrieve.php?');
     const json = await response.json();
     const array = json.data;
-    array.map((x)=>{
+    array.map((x) => {
         appendData(x);
     })
 
-    console.log(array);
+
+    var cartButtons = document.querySelectorAll('.cart-button');
+
+    for (const cartButton of cartButtons) {
+        cartButton.addEventListener('click', async function (event) {
+            console.log('clicked');
+            const product_id = cartButton.id;
+            console.log(product_id);
+            const response = await fetch('https://testing1.thestrategybook.com/deepak-ecommerce/server/api/products/sarees/retrieve.php?productid=' + product_id);
+            const json = await response.json();
+            const object = json.data;
+            console.log(object);
+            const data = {
+                "product_category": "sarees",
+                "productid": object.productid,
+                "selected_quantity": object.quantity,
+                "selected_size": object.size,
+                "selected_colors": object.colors,
+                "selected_length": object.length,
+                "selected_width": object.width,
+            }
+
+            fetch('https://testing1.thestrategybook.com/deepak-ecommerce/server/api/user/cart/add.php', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(data),
+            })
+                .then(response => response.json())
+                .then(data => {
+                    console.log('Success:', data);
+                })
+                .catch((error) => {
+                    console.error('Error:', error);
+                });
+        })
+    }
 });
+
